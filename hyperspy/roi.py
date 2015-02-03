12b5cacd08b4f4ab39e41fe5ab9ad9eebe098de1
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-02-03 12:08:23 +0100

    NEW: Interactive ROIs
    This commit establishes the connection between ROIs and widgets by
    adding a new class BaseInteractiveROI, which contains the interface for
    having a ROI have a corresponding widget. It also tries to supply a
    reasonable default implementation of this interface, but several
    functions will need to be overridden (raises NotImplementedError if
    not).

--- hyperspy/roi.py
+++ hyperspy/roi.py
@@ -1,7 +1,10 @@
 import traits.api as t
 import numpy as np
 from hyperspy.events import Events, Event
+import hyperspy.interactive
 from hyperspy.axes import DataAxis
+from hyperspy.drawing import widgets
+                                     
 
 
 class BaseROI(t.HasTraits):
@@ -114,7 +117,102 @@ class BaseROI(t.HasTraits):
         return axes, ax
 
 
-class BasePointROI(BaseROI):
+class BaseInteractiveROI(BaseROI):
+    def __init__(self):
+        super(BaseInteractiveROI, self).__init__()
+        self.widgets = set()
+    
+    def update(self):
+        if t.Undefined not in np.ravel(self.coords):
+            if not self.events.roi_changed.suppress:
+                self._update_widgets()
+            self.events.roi_changed.trigger(self)
+
+    def _update_widgets(self, exclude=set()):
+        if not isinstance(exclude, set):
+            exclude = set(exclude)
+        for w in self.widgets - exclude:
+            with w.events.suppress:
+                self._apply_roi2widget(w)
+            
+    def _apply_roi2widget(self, widget):
+        raise NotImplementedError()
+
+    def interactive(self, signal, navigation_signal="same", out=None):
+        if navigation_signal == "same":
+            navigation_signal = signal
+        if navigation_signal is not None:
+            self.add_widget(navigation_signal)
+        if out is None:
+            return hyperspy.interactive.interactive(self.__call__, 
+                                         event=self.events.roi_changed,
+                                         signal=signal)
+        else:
+            return hyperspy.interactive.interactive(self.__call__, 
+                                         event=self.events.roi_changed,
+                                         signal=signal, out=out)
+            
+    def _set_coords_from_widget(self, widget):
+        c = widget.coordinates
+        s = widget._get_size_in_axes()
+        self.coords = zip(c, c+s)
+
+    def _on_widget_change(self, widget):
+        with self.events.suppress:
+            self._bounds_check = False
+            try:
+                self._set_coords_from_widget(widget)
+            finally:
+                self._bounds_check = True
+        self._update_widgets(exclude=(widget,))
+        self.events.roi_changed.trigger(self)
+        
+    def _get_widget_type(self, axes, signal):
+        raise NotImplementedError()
+
+    def add_widget(self, signal, axes=None, widget=None, color='green'):
+        axes, ax = self._parse_axes(axes, signal.axes_manager, signal._plot)
+        if widget is None:
+            widget = self._get_widget_type(axes, signal)(signal.axes_manager)
+            widget.color = color
+        
+        # Remove existing ROI, if it exsists and axes match
+        if self.signal_map.has_key(signal) and \
+                self.signal_map[signal][1] == axes:
+            self.remove_widget(signal)
+        
+        if axes is not None:
+            # Set DataAxes
+            widget.axes = axes
+        with widget.events.suppress:
+            self._apply_roi2widget(widget)
+        if widget.ax is None:
+            widget.set_mpl_ax(ax)
+            
+        # Connect widget changes to on_widget_change
+        widget.events.changed.connect(self._on_widget_change)
+        # When widget closes, remove from internal list
+        widget.events.closed.connect(self._remove_widget)
+        self.widgets.add(widget)
+        self.signal_map[signal] = (widget, axes)
+        return widget
+        
+    def _remove_widget(self, widget):
+        widget.events.closed.disconnect(self._remove_widget)
+        widget.events.changed.disconnect(self._on_widget_change)
+        widget.close()
+        for signal, w in self.signal_map.iteritems():
+            if w == widget:
+                self.signal_map.pop(signal)
+                break
+        
+    def remove_widget(self, signal):
+        if self.signal_map.has_key(signal):
+            w = self.signal_map.pop(signal)[0]
+            self._remove_widget(w)
+
+
+class BasePointROI(BaseInteractiveROI):
     def _set_coords_from_widget(self, widget):
         c = widget.coordinates
         self.coords = zip(c)
@@ -137,13 +235,37 @@ class Point1DROI(BasePointROI):
         if self.coords != value:
             self.value = value[0,0]
 
+    def _apply_roi2widget(self, widget):
+        widget.coordinates = self.value
+        
+    def _get_widget_type(self, axes, signal):
+        if axes[0].navigate:
+            plotdim = len(signal._plot.navigator_data_function().shape)
+            axdim = signal.axes_manager.navigation_dimension
+            idx = signal.axes_manager.navigation_axes.index(axes[0])
+        else:
+            plotdim = len(signal._plot.signal_data_function().shape)
+            axdim = signal.axes_manager.signal_dimension
+            idx = signal.axes_manager.signal_axes.index(axes[0])
+        
+        if plotdim == 2:  # Plot is an image
+            # axdim == 1 and plotdim == 2 indicates "spectrum stack"
+            if idx == 0 and axdim != 1:    # Axis is horizontal
+                return widgets.DraggableVerticalLine
+            else:  # Axis is vertical
+                return widgets.DraggableHorizontalLine
+        elif plotdim == 1:  # It is a spectrum
+            return widgets.DraggableVerticalLine
+        else:
+            raise ValueError("Could not find valid widget type")
+
     def __repr__(self):
         return "%s(value=%f)" % (
             self.__class__.__name__,
             self.value)
 
 
-class Point2DROI(BaseROI):
+class Point2DROI(BaseInteractiveROI):
     x, y = (t.CFloat(t.Undefined),) * 2
     
     def __init__(self, x, y):
@@ -162,13 +284,19 @@ class Point2DROI(BaseROI):
     def _y_changed(self, old, new):
         self.update()
 
+    def _apply_roi2widget(self, widget):
+        widget.coordinates = (self.x, self.y)
+        
+    def _get_widget_type(self, axes, signal):
+        return widgets.DraggableSquare
+
     def __repr__(self):
         return "%s(value=%f)" % (
             self.__class__.__name__,
             self.value)
         
 
-class SpanROI(BaseROI):
+class SpanROI(BaseInteractiveROI):
     left, right = (t.CFloat(t.Undefined),) * 2
     
     def __init__(self, left, right):
@@ -197,6 +325,12 @@ class SpanROI(BaseROI):
         else:
             self.update()
 
+    def _apply_roi2widget(self, widget):
+        widget.set_bounds(left=self.left, right=self.right)
+        
+    def _get_widget_type(self, axes, signal):
+        return widgets.DraggableResizableRange
+
     def __repr__(self):
         return "%s(left=%f, right=%f)" % (
             self.__class__.__name__,
@@ -204,7 +338,7 @@ class SpanROI(BaseROI):
             self.right)
 
 
-class RectangularROI(BaseROI):
+class RectangularROI(BaseInteractiveROI):
     top, bottom, left, right = (t.CFloat(t.Undefined),) * 4
 
     def __init__(self, left, top, right, bottom):
@@ -246,6 +380,13 @@ class RectangularROI(BaseROI):
             self.left = old
         else:
             self.update()
+
+    def _apply_roi2widget(self, widget):
+        widget.set_bounds(left=self.left, bottom=self.bottom, 
+                          right=self.right, top=self.top)
+        
+    def _get_widget_type(self, axes, signal):
+        return widgets.ResizableDraggableRectangle
 
     def __repr__(self):
         return "%s(left=%f, top=%f, right=%f, bottom=%f)" % (
