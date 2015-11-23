a2ad4da75dbff4823901b891a75372e076043dcf
vidartf <vidartf@gmail.com>
2015-11-23 16:39:03 +0100

    Initial blockfile reader

--- hyperspy/io_plugins/__init__.py
+++ hyperspy/io_plugins/__init__.py
@@ -19,8 +19,8 @@
 
 from hyperspy import messages
 from hyperspy.io_plugins import (msa, digital_micrograph, fei, mrc,
-                                 ripple, tiff)
-io_plugins = [msa, digital_micrograph, fei, mrc, ripple, tiff]
+                                 ripple, tiff, blockfile)
+io_plugins = [msa, digital_micrograph, fei, mrc, ripple, tiff, blockfile]
 try:
     from hyperspy.io_plugins import netcdf
     io_plugins.append(netcdf)
